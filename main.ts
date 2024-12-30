46bd8ece07d730d911fdda1440a5f430f87844df
Fast4word <152832715+Fast4word@users.noreply.github.com>
2024-12-31 09:47:18 +1300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,18 @@
+player.onChat("mode-creative", function () {
+    gameplay.setGameMode(
+    CREATIVE,
+    mobs.target(NEAREST_PLAYER)
+    )
+})
 player.onChat("mode-survival", function () {
     gameplay.setGameMode(
     SURVIVAL,
+    mobs.target(LOCAL_PLAYER)
+    )
+})
+player.onChat("mode-adventure", function () {
+    gameplay.setGameMode(
+    ADVENTURE,
     mobs.target(LOCAL_PLAYER)
     )
 })
